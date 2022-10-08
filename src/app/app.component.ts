5f7b03dc18d95b3620d30bfebd348d628e806b4a
mnacakan007 <mnacakan007@gmail.com>
2022-10-08 14:57:21 +0400

    initial state 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  standalone: true,
-  imports: [MatToolbarModule, RouterModule],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular-standalone-components-example';

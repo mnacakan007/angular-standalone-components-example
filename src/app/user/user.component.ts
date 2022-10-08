d88084186926d233afafc9200fc97f7a21e76511
mnacakan007 <mnacakan007@gmail.com>
2022-10-08 14:54:26 +0400

    initial state

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { MatCardModule } from "@angular/material/card";
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss'],
-  standalone: true,
-  imports: [MatCardModule]
+  styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
 
